ce28a9895d9c9c95a739209d0aed1b5410efaa3d
Lara <laronious@ciencias.unam.mx>
2025-02-11 17:59:08 -0600

    Rama pruebas.

--- Snoc.hs
+++ Snoc.hs
@@ -18,19 +18,26 @@ import Data
 
 -- | headS. Función que obtiene el primer elemento de la lista.
 headS :: ListS a -> a
-headS = error "D:"
+headS NilS = error "La lista es vacía."
+headS (Snoc NilS a) = a
+headS (Snoc l _) = headS l
 
 -- | tailS. Función que obtiene la lista sin el primer elemento.
 tailS :: ListS a -> ListS a
-tailS = error "D:"
+tailS NilS = error "La lista es vacía"
+tailS (Snoc NilS _) = NilS
+tailS (Snoc l a) = Snoc (tailS l) a
 
 -- | initS. Función que obtiene la lista sin el ultimo elemento.
 initS :: ListS a -> ListS a
-initS = error "D:"
+initS NilS = error "La lista es vacía"
+initS (Snoc NilS _) = NilS
+initS (Snoc l a) = l
 
 -- | lastS. Función que obtiene el último elemento de la lista.
 lastS :: ListS a -> a
-lastS = error "D:"
+lastS NilS = error "La lista es vacía."
+lastS (Snoc l a) = a
 
 -- | nthElementS. Función que regresa el n-ésimo elemento de la lista.
 nthElementS :: Int -> ListS a -> Maybe a
@@ -39,16 +46,19 @@ nthElementS = error "D:"
 -- | addFirstS. Función que obtiene la lista donde el primer elemento es el
 -- | elemento dado.
 addFirstS :: a -> ListS a -> ListS a
-addFirstS = error "D:"
+addFirstS a NilS = Snoc NilS a
+addFirstS a (Snoc l b) = Snoc (addFirstS a l) b
 
 -- | addLastS. Función que obtiene la lista donde el último elemento es el
 -- | elemento dado.
 addLastS :: a -> ListS a -> ListS a
-addLastS = error "D:"
+addLastS a NilS = Snoc NilS a
+addLastS a l = Snoc l a
 
 -- | reverseS. Función que obtiene la reversa de la lista.
 reverseS :: ListS a -> ListS a
-reverseS = error "D:"
+reverseS NilS = NilS
+reverseS (Snoc l a) = addLastS a l
 
 -- | appendS. Función que obtiene la concatenación de dos listas.
 appendS :: ListS a -> ListS a -> ListS a
